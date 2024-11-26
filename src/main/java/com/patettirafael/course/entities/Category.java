f542aee578e568db28e2b95aeb4c7ca1bdc774a1
rpatetti <patetti.rafael@gmail.com>
2024-11-26 15:42:07 -0300

    Product

--- src/main/java/com/patettirafael/course/entities/Category.java
+++ src/main/java/com/patettirafael/course/entities/Category.java
@@ -1,13 +1,15 @@
 package com.patettirafael.course.entities;
 
 import java.io.Serializable;
-import java.util.Objects;
+import java.util.HashSet;
+import java.util.Set;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 
 @Entity
 @Table(name = "tb_category")
@@ -19,48 +21,60 @@ public class Category implements Serializable {
     private Long id;
     private String name;
     
+    @Transient
+    private Set<Product> products = new HashSet<>();
+    
     public Category() {
-    }
+	}
 
-    public Category(Long id, String name) {
-	super();
-	this.id = id;
-	this.name = name;
-    }
+	public Category(Long id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    @Override
-    public int hashCode() {
-	return Objects.hash(id);
-    }
+	public Set<Product> getProducts() {
+		return products;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-	if (this == obj)
-	    return true;
-	if (obj == null)
-	    return false;
-	if (getClass() != obj.getClass())
-	    return false;
-	Category other = (Category) obj;
-	return Objects.equals(id, other.id);
-    }
-    
-    
-    
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Category other = (Category) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 }
