739dea0533d04293731b4458c2ff5432082ada9a
rpatetti <patetti.rafael@gmail.com>
2024-11-27 20:53:48 -0300

    Payment, one-to-one association

--- src/main/java/com/patettirafael/course/config/TestConfig.java
+++ src/main/java/com/patettirafael/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.patettirafael.course.entities.Category;
 import com.patettirafael.course.entities.Order;
 import com.patettirafael.course.entities.OrderItem;
+import com.patettirafael.course.entities.Payment;
 import com.patettirafael.course.entities.Product;
 import com.patettirafael.course.entities.User;
 import com.patettirafael.course.entities.enums.OrderStatus;
@@ -84,6 +85,11 @@ public class TestConfig implements CommandLineRunner {
 	OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 	
 	orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));	
+	
+	Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+	o1.setPayment(pay1);
+	
+	orderRepository.save(o1);
 	
     }
     
