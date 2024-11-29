cdcd04f2cdee12a849d49cd2cbc77b4448f29b76
rpatetti <patetti.rafael@gmail.com>
2024-11-29 19:30:55 -0300

    Exception handling - findById

--- src/main/java/com/patettirafael/course/services/UserService.java
+++ src/main/java/com/patettirafael/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.patettirafael.course.entities.User;
 import com.patettirafael.course.repositories.UserRepository;
+import com.patettirafael.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
     
     public User findById(Long id) {
 	Optional<User> obj = repository.findById(id);
-	return obj.get();
+	return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
     
     public User insert(User obj) {
@@ -30,5 +31,17 @@ public class UserService {
     
     public void delete(long id) {
 	repository.deleteById(id);
+    }
+    
+    public User update(Long id, User obj) {
+	User entity = repository.getReferenceById(id);
+	updateData(entity, obj);
+	return repository.save(entity);
+    }
+
+    private void updateData(User entity, User obj) {
+	entity.setName(obj.getName());
+	entity.setEmail(obj.getEmail());
+	entity.setPhone(obj.getPhone());
     }
 }
